e57eff79384f9d6984be3d075ba54699e2c77256
LukaLiegis <luka.liegis@gmail.com>
2024-03-06 13:50:50 +0200

    Commit main.py

--- main.py
+++ main.py
@@ -3,5 +3,15 @@ import config
 import pandas as pd
 from binance.client import Client
 
-client = Client(config.api_key, config.api_secret)
+client = Client(config.api_key, config.api_secret, testnet = False)
+
+symbol = "DOGEUSDT"
+
+class BinanceData:
+    def __init__(self):
+        self.symbol = symbol
+    async def get_orderbook(self):
+        return client.get_order_book(symbol = symbol)
+    async def get_trades(self):
+        return client.get_recent_trades(symbol = symbol)
 
