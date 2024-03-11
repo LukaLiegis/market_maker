619a58209cb6018a5d98181edb53cc430e8cf11f
LukaLiegis <luka.liegis@gmail.com>
2024-03-11 16:50:56 +0200

    Commit data.py and main.py

--- src/exchanges/binance/data.py
+++ src/exchanges/binance/data.py
@@ -1,41 +1,20 @@
-from binance.client import Client
-from src.sharedstate import SharedState
+import config
+from binance import Client
 
-class BinanceData:
-  def __init__(self, api_key, api_secret, testnet=False):
+client = Client(api_key=config.api_key)
+
+
+def orderbook():
     """
-    Initializes the BinanceData class with API credentials and optional testnet flag.
+    Fetches the orderbook for the symbol
+    :return: dictionary containing the orderbook
     """
-    self.client = Client(api_key, api_secret, testnet=testnet)
+    return client.get_order_book(symbol=config.ticker, limit=config.limit)
 
-  async def get_orderbook(self):
+
+def trades():
     """
-    Gets the current order book for a specific symbol using the synchronous API.
-
-    Args:
-      symbol: The symbol of the cryptocurrency pair (e.g., "BTCUSDT").
-
-    Returns:
-      A dictionary containing the order book data.
+    Gets recent trades for the symbol
+    :return:
     """
-    return self.client.get_orderbook(symbol = self.symbol, limit = limit)
-
-  async def get_candlesticks(self, interval, limit):
-    """
-    Gets candlestick data for a specific symbol using the asynchronous API.
-
-    Args:
-      symbol: The symbol of the cryptocurrency pair (e.g., "BTCUSDT").
-      interval: The candlestick interval (e.g., Client.KLINE_INTERVAL_1MINUTE).
-      limit: The maximum number of candlesticks to retrieve.
-
-    Returns:
-      A list of candlestick data points.
-    """
-    return self.client.get_klines(symbol = self.symbol, interval = interval, limit=limit)
-
-  async def trades(self, limit):
-      """
-      Get a list of recent trades
-      """
-      return self.client.get_recent_trades(symbol = self.symbol, limit = limit)
+    return client.get_recent_trades(symbol=config.ticker, limit=config.limit)
