0c8cb09bb2d62648a5c2a0e98bd1937b427218b8
LukaLiegis <luka.liegis@gmail.com>
2024-03-14 16:51:23 +0200

    Commit endpoints.py

--- src/exchanges/binance/endpoints.py
+++ src/exchanges/binance/endpoints.py
@@ -3,4 +3,4 @@ from dataclasses import dataclass
 @dataclass
 class WsStreamLinks:
     SPOT_PUBLIC_STREAM = "wss://stream.binance.com:9443"
-    FUTURES_PUBLIC_STREAM = "wss://fstream.binance.com"
+    FUTURES_PUBLIC_STREAM = "wss://fstream.binance.com/ws"
